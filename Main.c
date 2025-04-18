911c9c664dc102d7011c66baafa821ac34976f9a
Aelin Med <aelinmed@gmail.com>
2025-04-18 01:32:52 +0000

    first part + adding compiling button

--- Main.c
+++ Main.c
@@ -6,6 +6,7 @@
 int main(void)
 {
     char *args[MAX_LINE/2 + 1]; /* command line arguments */
+    char input[MAX_LINE]; // user input
     int should_run = 1;         /* flag to determine when to exit program */
 
     while (should_run) {
@@ -18,6 +19,21 @@ int main(void)
          * (2) the child process will invoke execvp()
          * (3) parent will invoke wait() unless command included &
          */
+
+        // Read the input from the user 
+        if (fgets(input, MAX_LINE, stdin) == NULL) {
+            continue;
+        }
+
+        // Remove trailing newline character
+        input[strcspn(input, "\n")] = '\0';
+
+        // Exit command
+        if (strcmp(input, "exit") == 0) {
+            should_run = 0;
+            continue;
+        }
+
     }
 
     return 0;
