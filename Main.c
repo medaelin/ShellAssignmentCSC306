589151cb4ef39d06897157415d7d7d670dc94a7d
Aelin Med <w10087858@usm.edu>
2025-04-17 20:27:28 -0500

    Starter code and testing

--- Main.c
+++ Main.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
+
+#define MAX_LINE 80 /* The maximum length command */
+
+int main(void)
+{
+    char *args[MAX_LINE/2 + 1]; /* command line arguments */
+    int should_run = 1;         /* flag to determine when to exit program */
+
+    while (should_run) {
+        printf("osh> ");
+        fflush(stdout);
+
+        /**
+         * After reading user input, the steps are:
+         * (1) fork a child process using fork()
+         * (2) the child process will invoke execvp()
+         * (3) parent will invoke wait() unless command included &
+         */
+    }
+
+    return 0;
+}
